531c7a9f19b52d8f9d7272b116add56082c1a38b
rmccabe <rmccabe@mit.edu>
2019-08-19 23:33:35 -0700

    unused pins set as outputs

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,22 +1,30 @@
-// receiver.pde
+// receiver.ino
 //
-// Simple example of how to use VirtualWire to receive messages
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-//
+// Use VirtualWire to receive messages and trigger a buzzer
 // See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
+
+// Original author: Mike McCauley (mikem@airspayce.com)
 // Copyright (C) 2008 Mike McCauley
-// $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
+// Modifications: Rebecca McCabe, 2019
 
 #include <VirtualWire.h>
 int buzzer=10;
 int led = 13;
+
+int unused [] = {0,1,2,3,4,5,6,7,8,9, 12, 14,15,16,17,18,19,20,21,22,23};
 
 void setup()
 {
     pinMode(led,OUTPUT);
     pinMode(buzzer,OUTPUT);
     analogWrite(buzzer,LOW);
+
+    // save power by setting unused pins as outputs
+    for(i=0;i < sizeof unused/sizeof unused[0]; i++) {
+       int pin = unused[i];
+       pinMode(pin, OUTPUT);
+    }
+
     // Initialise the IO and ISR
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);	 // Bits per sec
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,17 +1,18 @@
-// transmitter.pde
+// transmitter.ino
 //
-// Simple example of how to use VirtualWire to transmit messages
-// Implements a simplex (one-way) transmitter with an TX-C1 module
-//
+// Read potentiometer to determine volume and use 
+// VirtualWire to transmit the volume when button pressed
 // See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
+
+// Original author: Mike McCauley (mikem@airspayce.com)
 // Copyright (C) 2008 Mike McCauley
-// $Id: transmitter.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
+// Modifications: Rebecca McCabe, 2019
 
 #include <VirtualWire.h>
 int button = 8;
 int pot = 14;
 int led = 13;
+int unused [] = {0,1,2,3,4,5,6,7, 9,10,11, 15,16,17,18,19,20,21,22,23};
 
 float minVolume = 110.0;
 float maxVolume = 255.0;
@@ -21,12 +22,15 @@ float slope = (maxVolume - minVolume) / (maxPotReading - minPotReading);
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
-    Serial.println("setup");
-
     // Initialise the IO and ISR
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);	 // Bits per sec
+
+    // save power by setting unused pins as outputs
+    for(i=0;i < sizeof unused/sizeof unused[0]; i++) {
+       int pin = unused[i];
+       pinMode(pin, OUTPUT);
+    }
 
     pinMode(led, OUTPUT);
     pinMode(button, INPUT);
@@ -36,12 +40,9 @@ void setup()
 void loop()
 {
     int reading = analogRead(pot);
-    Serial.println(reading);
     int vol = minVolume + slope * (reading - minPotReading);
-    Serial.println(vol);
     char buffer[7];
     dtostrf(vol, 7, 0, buffer);
-    //const char *msg = "hello";
     char *msg = buffer;
     
     bool press = digitalRead(button);
